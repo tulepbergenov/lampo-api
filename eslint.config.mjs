0344e592117ca4bb84764e058e6aa2afe8a8430b
Daulet Tulepbergenov <daulet6368@gmail.com>
2024-12-30 20:51:32 +0500

    feat: init node app

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import pluginJs from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
