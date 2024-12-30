73f5bcf36f5a1ec5a38f629494b96cedae8b6d37
Daulet Tulepbergenov <daulet6368@gmail.com>
2024-12-30 21:02:51 +0500

    feat: add express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello, world!");
+import express from "express";
+
+const expressApp = express();
+
+const PORT = 3080;
+
+expressApp.get("/hello", (_, res) => {
+  res.send("Hello World!");
+});
+
+expressApp.listen(PORT, () => {
+  console.info(`Listening at http://localhost:${PORT}`);
+});
